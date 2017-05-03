5182cd0c2935038da54a3bdaa53fd16ad8907cb7
Cleber Castilho <cleber.castilho@gmail.com>
2017-05-03 18:04:43 -0300

    Adicionando API para consumo via http rest pelo angular2

---
+++ AlunosAPI/Controllers/AlunoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AlunosAPI.Controllers
+{
+    public class AlunoController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, new { sucesso = true });
+        }
+
+        public HttpResponseMessage Delete(int id)
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, new { sucesso = true });
+        }
+    }
+}
---
+++ AlunosAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AlunosAPI.WebApiApplication" Language="C#" %>
